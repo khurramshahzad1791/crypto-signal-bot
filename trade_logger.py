f87b7dfd0ffcbd7bc25a62701dd15fcec86d61d6
khurramshahzad1791 <khurramshahzad1791@gmail.com>
2026-03-15 05:34:13 +0500

    Update trade_logger.py

--- trade_logger.py
+++ trade_logger.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Float, DateTime, Integer, Boolean
+from sqlalchemy import create_engine, Column, String, Float, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
